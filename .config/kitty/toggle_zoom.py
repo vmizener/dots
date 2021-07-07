6997ee2abdf2440269ef809572e181f7ef7d8001
Raymond von Mizener <vmizener@gmail.com>
2021-07-07 08:20:29 -0700

    Added zoom shortkey to kitty

---
+++ .config/kitty/toggle_zoom.py
@@ -0,0 +1,17 @@
+def main(args):
+    pass
+
+
+from kittens.tui.loop import debug
+from kittens.tui.handler import result_handler
+
+
+@result_handler(no_ui=True)
+def handle_result(args, answer, target_window_id, boss):
+    tab = boss.active_tab
+    debug("hi")
+    if tab is not None:
+        if tab.current_layout.name == "stack":
+            tab.last_used_layout()
+        else:
+            tab.goto_layout("stack")
