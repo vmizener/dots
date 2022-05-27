de8d2547fe51378aa67169a38e862301379f08b4
Raymond von Mizener <vmizener@gmail.com>
2022-05-27 13:35:41 -0700

    Added sway background, made idle dimness a variable

--- .config/sway/swayidle.sh
+++ .config/sway/swayidle.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-TIMEOUT_DIM=60      #  1 min:   Dim screen if on battery
+TIMEOUT_DIM=120     #  2 min:   Dim screen if on battery
 TIMEOUT_LOCK=600    # 10 min:   Lock (must be before turning off screen)
 TIMEOUT_BLACK=900   # 15 min:   Turn off screen
+
+DIM_BRIGHTNESS=10   # Dim to 10% brightness
 
 # Use tmp files to store state as envvars don't work
 TMP_SCREENBRIGHTNESS=/tmp/swayidle_screenbrightness
@@ -15,7 +17,7 @@ AC_STATUS=/sys/class/power_supply/AC/online
 killall -q swayidle
 swayidle -w \
     timeout $TIMEOUT_DIM \
-        "echo \$(brightnessctl g) > $TMP_SCREENBRIGHTNESS; if [ \$(cat $AC_STATUS) -eq 0 ]; then brightnessctl s 5; fi" \
+        "echo \$(brightnessctl g) > $TMP_SCREENBRIGHTNESS; if [ \$(cat $AC_STATUS) -eq 0 ]; then brightnessctl s $DIM_BRIGHTNESS; fi" \
         resume "brightnessctl s \$(cat $TMP_SCREENBRIGHTNESS)" \
     timeout $TIMEOUT_LOCK \
         'swaylock' \
