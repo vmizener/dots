704435a2d854e29dd70cd82ed8fbdc9647d9e0c4
Raymond von Mizener <vmizener@gmail.com>
2022-09-02 16:44:54 -0700

    Added window picker to sway; pickers now close when focus is lost; removed extraneous flags in config

--- .config/sway/hotkeys.sh
+++ .config/sway/hotkeys.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 SCRIPTNAME=$(basename $0)
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+TITLE="Sway: Cheat Sheet"
 HOTKEYS_TXT="${SCRIPTPATH}/hotkeys.txt"
 if [[ -n $SR ]]; then
     # Run FZF and exit if `SR` is set
@@ -9,8 +10,25 @@ if [[ -n $SR ]]; then
 else
     # Otherwise relaunch script in foot with `SR` exported
     WIDTH="$(( $(wc -L ${HOTKEYS_TXT} | cut -f1 -d' ')+3 ))"
-    SR=true foot --app-id prompt \
+    SR=true foot \
+        --app-id prompt \
+        --title "${TITLE}" \
         -W "${WIDTH}x10" \
         -f 'monospace:size=12' \
         "${SCRIPTPATH}/${SCRIPTNAME}" &
+    # Kill when focus is lost
+    pid=$!
+    while kill -0 $pid 2&>/dev/null; do
+        pattern="
+            recurse(.nodes[]?) | 
+            recurse(.floating_nodes[]?) | 
+            select(.pid==${pid}) | 
+            .focused
+        "
+        is_focused=$(swaymsg -t get_tree | jq -r "$pattern")
+        if [[ $is_focused == 'false' ]]; then
+            kill -9 $pid
+        fi
+        sleep 0.1
+    done &
 fi
---
+++ .config/sway/window_picker.sh
@@ -0,0 +1,85 @@
+#!/usr/bin/env bash
+SCRIPTNAME=$(basename $0)
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+IFS=$'\n'
+
+header="Select window"
+if [[ -n $fifo ]]; then
+    str=$(cat $fifo)
+    rm -rf $fifo
+
+    selection=$(printf $str | fzf --header=$header)
+
+    id=$(echo $selection | cut -d ":" -f1)
+    if [[ -z $id ]]; then
+        exit
+    fi
+
+    swaymsg "[con_id=$id]" focus
+    exit
+else
+    windows=(
+        $(swaymsg -t get_tree | jq -r '
+            recurse(.nodes[]?) | 
+            recurse(.floating_nodes[]?) | 
+            select(.type=="con"), select(.type=="floating_con") | 
+            select((.app_id != null) or .name != null) | 
+            {id, app_id, name} | .id, .app_id, .name
+        ')
+    )
+
+    str=""
+    columns="${#header}"
+    lines=0
+    for ((i=0; i<"${#windows[@]}"; i=i+3,lines++)); do
+        id="${windows[i]}"
+        app_id="${windows[i+1]}"
+        name="${windows[i+2]}"
+
+        if [[ $app_id = "null" ]]; then
+            app_id=""
+        fi
+        if [[ $name = "null" ]]; then
+            name=""
+        fi
+        building_string="$id:$app_id:$name"
+        if [[ ${#building_string} -gt $columns ]]; then
+            columns=${#building_string}
+        fi
+        str="$str$building_string\n" 
+    done
+    echo $str
+
+    lines=$((lines+3))
+    columns=$((columns+3))
+    if [[ columns -gt 100 ]]; then
+        columns=100
+    fi
+
+    fifo=/tmp/sts-$(date +%s)
+    mkfifo $fifo
+    fifo=$fifo foot \
+        --app-id prompt \
+        --title "Sway: Window Picker" \
+        -W "${columns}x${lines}" \
+        -f 'monospace:size=12' \
+        "${SCRIPTPATH}/${SCRIPTNAME}" &
+    pid=$!
+    echo -n $str > $fifo
+
+    # Kill when focus is lost
+    while kill -0 $pid 2&>/dev/null; do
+        pattern="
+            recurse(.nodes[]?) | 
+            recurse(.floating_nodes[]?) | 
+            select(.pid==${pid}) | 
+            .focused
+        "
+        is_focused=$(swaymsg -t get_tree | jq -r "$pattern")
+        if [[ $is_focused == 'false' ]]; then
+            kill -9 $pid
+        fi
+        sleep 0.1
+    done &
+fi
+
