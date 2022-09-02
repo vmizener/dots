4afc221273c9fb47b893833a69183f43fae9af1f
Raymond von Mizener <vmizener@gmail.com>
2022-09-02 14:19:35 -0700

    Sway hotkey cheatsheet now uses fzf in foot term

--- .config/sway/hotkeys.sh
+++ .config/sway/hotkeys.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
-
-cat ~/.config/sway/hotkeys.txt | grep '[^\n]' | wofi -Ii --show=dmenu >/dev/null
+SCRIPTNAME=$(basename $0)
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+HOTKEYS_TXT="${SCRIPTPATH}/hotkeys.txt"
+if [[ -n $SR ]]; then
+    # Run FZF and exit if `SR` is set
+    cat "${HOTKEYS_TXT}" | grep '[^\n]' | fzf
+    exit
+else
+    # Otherwise relaunch script in foot with `SR` exported
+    WIDTH="$(( $(wc -L ${HOTKEYS_TXT} | cut -f1 -d' ')+3 ))"
+    SR=true foot --app-id prompt \
+        -W "${WIDTH}x10" \
+        -f 'monospace:size=12' \
+        "${SCRIPTPATH}/${SCRIPTNAME}" &
+fi
