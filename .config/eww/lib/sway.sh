79739b7661785b2e4eed889fbacdbad93e614caa
Raymond von Mizener <vmizener@gmail.com>
2023-05-05 15:32:59 -0700

    Eww initial alt-tab implementation

--- .config/eww/lib/sway.sh
+++ .config/eww/lib/sway.sh
@@ -11,7 +11,7 @@ function workspace::list_by_output() {
     )
 }
 
-function workspace::subscribe() {
+function workspace::focus_subscriber() {
     workspace::list_by_output
     swaymsg -t subscribe -m '["workspace"]' | jq --unbuffered -rc '.change' |
         while read -r event; do
@@ -20,4 +20,40 @@ function workspace::subscribe() {
             fi
             workspace::list_by_output
         done
+}
+
+function container::snapshot_subscriber() {
+    SNAPSHOT_DIR="${HOME}/.cache/eww-snapshots"
+    mkdir -p "$SNAPSHOT_DIR"
+    rm -f "$SNAPSHOT_DIR"/*.jpg
+    swaymsg -t subscribe -m '["window"]' |
+        jq --unbuffered -rc '[
+            .change,
+            .container.id,
+            .container.pid,
+            .container.rect.x,
+            .container.rect.y,
+            .container.rect.width,
+            .container.rect.height
+        ] | @tsv' |
+        while read -r line; do
+            line=( $(echo "$line") )
+            event="${line[1]}"
+            if [ "$event" = "focus" ] || [ "$event" = "new" ]; then
+                window="window-${line[2]}-${line[3]}.jpg"
+                rect_x="${line[4]}"
+                rect_y="${line[5]}"
+                rect_w="${line[6]}"
+                rect_h="${line[7]}"
+                grim -g "${rect_x},${rect_y} ${rect_w}x${rect_h}" "$SNAPSHOT_DIR/$window"
+            elif [ "$event" = "close" ]; then
+                rm -f "$SNAPSHOT_DIR/$window"
+            fi
+    done
+
+}
+
+function container::list_container_snapshots() {
+    SNAPSHOT_DIR="${HOME}/.cache/eww-snapshots"
+    echo $(ls "${SNAPSHOT_DIR}"/*.jpg | jq -R | jq -cs)
 }
