4d31bf4c829e08a523fa135f5ed75543d0c4fb60
Raymond von Mizener <rvonmizener@google.com>
2023-08-21 22:06:25 +0000

    Neovim: support pyformat styling

--- .config/nvim/after/ftplugin/python.lua
+++ .config/nvim/after/ftplugin/python.lua
@@ -1,16 +1,45 @@
--- Autoformat on write
+-- Helper function
+local function reformat (executable)
+    local view = vim.fn.winsaveview()
+    vim.cmd('silent execute ":%!' .. executable .. '"')
+    if vim.v['shell_error'] > 0 then
+        -- If there are errors, put them in the quicklist and undo the formatting
+        vim.cmd("cexpr getline(1, '$')->map({ idx, val -> val->substitute('<standard input>', expand('%'), '') })")
+        vim.cmd('silent undo')
+        vim.cmd('cwindow')
+    end
+    vim.fn.winrestview(view)
+end
+
 vim.api.nvim_create_augroup('PythonConfig', {})
-vim.api.nvim_create_autocmd('BufWritePre', {
+-- Set style settings if necessary
+vim.api.nvim_create_autocmd('BufEnter', {
     pattern = '*.py',
     callback = function ()
-        if vim.fn.exists(":Black") then
-            vim.cmd('silent execute ":Black"')
+        -- Use pyformat settings if we're using that
+        if vim.fn.executable('pyformat') == 1 then
+            local options = {
+                shiftwidth = 2,         -- Set indentation depth to 2 columns
+                tabstop = 2,            -- Set tabular length to 2 columns
+            }
+            for k, v in pairs(options) do vim.o[k] = v end
         end
     end,
     group = 'PythonConfig'
 })
---    -- Warn if python is missing required package
---    vim.fn.system({'pip', 'show', 'black'})
---    if vim.v['shell_error'] == 1 then
---        vim.api.nvim_echo({{"Python is missing 'black' package; install with `pip install black`", "WarningMsg"}}, true, {})
---    end
+-- Autoformat on write
+vim.api.nvim_create_autocmd('BufWritePre', {
+    pattern = '*.py',
+    callback = function ()
+        -- Use pyformat if present
+        if vim.fn.executable('pyformat') == 1 then
+            reformat('pyformat')
+        -- Fallback on black
+        elseif vim.fn.executable("black") then
+            reformat('black')
+        else
+            vim.api.nvim_echo({{"Missing python formatter! Install either pyformat or black via `pip`", "WarningMsg"}}, true, {})
+        end
+    end,
+    group = 'PythonConfig'
+})
